9599a633cb71ef77b0ed51f691d7c4b3c6dd37ad
jheniferlu <59872801+jheniferlu@users.noreply.github.com>
2020-01-30 08:54:09 -0300

    n4

--- fase02/ex04/minusculo.js
+++ fase02/ex04/minusculo.js
@@ -1,4 +1,4 @@
 function minusculo(palavra) {
-    const palavras = palavra
-    return palavra.substring(0,1).concat(palavra.substring(1).toUpperCase())
+    const palavras = palavra;
+    return palavra.substr(0,1).concat(palavra.substring(1).toUpperCase())
 }
