2970693e30ea0bb0484e669e75fc5c83ff9f0da1
jheniferlu <59872801+jheniferlu@users.noreply.github.com>
2020-02-06 09:00:07 -0300

    n2

---
+++ fase03/ex02/pares.js
@@ -0,0 +1,9 @@
+function parOuImpar(num,num2) {
+    var numero1 = num;
+    if(numero1 % 2 ===0){
+        return("Os numeros são pares")
+    }
+    else{
+        return ("Os numeros não são pares")
+    }
+}
