747e9ff43287c09766049a55b2fc9fdf7ef02850
jheniferlu <59872801+jheniferlu@users.noreply.github.com>
2020-02-06 10:36:57 -0300

    n1

--- fase03/ex01/nove.js
+++ fase03/ex01/nove.js
@@ -1,10 +1,3 @@
-function parOuImpar(numero){
-    var numero = (numero)
-    var total = numero % 2;
-    if(total==0){
-        return ("par")
-    }else{
-        return("impar")
-
-}
+function nove(numero){
+return numero.toString().substr(-1,1)==9
 }
