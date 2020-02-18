1f9734c1486b8f7e3f6e6f163f674d40e356176d
jheniferlu <59872801+jheniferlu@users.noreply.github.com>
2020-02-18 08:49:06 -0300

    n0

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,16 +1,6 @@
-function por42(num1, num2){
-    var result = null
-    var contador = 0
-    while (num1 <= num2){
-        if(num1 % 42 ===0){
-            contador = contador + 1
-        }if(contador ===2){
-            return(num1)
-        
-        }
-        num1++
-   } if(!result){
-       console.log("não encontrado")
-       return false 
-   }
+function entreNumeros(min, max){
+    while(min<= max){
+        console.log(min, max)
+        min++
+    }
 }
